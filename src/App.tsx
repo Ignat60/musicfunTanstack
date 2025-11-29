5dc7a387c13860b61047f9b87bebf43c9f73e552
Ignat60 <dedaignat@gmail.com>
2025-11-29 16:36:19 +0300

    chore:creat cliet and make first request for playlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useEffect } from "react";
 import "./App.css";
+import { client } from "./shared/api/client";
 
 function App() {
   useEffect(() => {
     (async function () {
-      const response = await fetch(
-        "https://musicfun.it-incubator.app/api/1.0/playlists",
-        {
-          headers: {
-            "api-key": "5dacb5c0-8de7-4786-90f4-2f8e4513b7e9",
-          },
-        }
-      );
-      const data = response.json();
+      const response = await client.GET("/playlists");
+      const data = response.data;
+
+      // const response = await fetch(
+      //   "https://musicfun.it-incubator.app/api/1.0/playlists",
+      //   {
+      //     headers: {
+      //       "api-key": "5dacb5c0-8de7-4786-90f4-2f8e4513b7e9",
+      //     },
+      //   }
+      // );
+      // const data = response.json();
+
       console.log(data);
     })();
   }, []);
